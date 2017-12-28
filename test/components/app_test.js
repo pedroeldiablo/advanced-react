e2cf13bc6504183b2fcb22f2927f4d98f3ce8dbd
Peter Williams <pedroeldiablo@yahoo.com>
2017-12-28 02:18:22 +0000

    up to form submit event

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,18 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
+//Use describe to group together similar tests
+//the first parameter is a string to help identify the component where the tests are carried out when you get the results i.e. could be Test the App Component
+describe('App', () => {
   let component;
 
   beforeEach(() => {
     component = renderComponent(App);
   });
-
-  it('renders something', () => {
-    expect(component).to.exist;
+  //Use 'it' to test a single attribute of a target. i.e. what exactly you are testing about 'App'
+  it('shows a comment box', () => {
+    //Use 'expect' to make an 'assertion' about a target.
+    expect(component.find('.comment-box')).to.exist;
   });
+
 });
