d4b0fccaa95b5fc79977a52210ca3a364ba11898
Peter Williams <pedroeldiablo@yahoo.com>
2018-01-02 00:42:02 +0000

    rebuilds the test_helper.js file with additional comments

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,36 +1,42 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
-import chai, { expect } from 'chai';
+import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 import chaiJquery from 'chai-jquery';
+
+//set up testing environment to run like a browser but in the command line
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+global.navigator = global.window.navigator;
+const $ = jquery(global.window);
+import chai, { expect } from 'chai';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
-
-chaiJquery(chai, chai.util, $);
-
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
+//build 'renderComponent' helper that should render a given react class
+function renderComponent(ComponentClass, props, state) {
+  const componentInstance = TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
     </Provider>
-  );
-
+    );
+//produces HTML wrapped in jquery call
   return $(ReactDOM.findDOMNode(componentInstance));
+
 }
 
-$.fn.simulate = function(eventName, value) {
-  if (value) {
+//build helper for simulating events
+$.fn.simulate = function (eventName, value) {
+  if(value) {
     this.val(value);
   }
   TestUtils.Simulate[eventName](this[0]);
-};
 
-export {renderComponent, expect};
+}
+
+//set-up chai-jquery
+chaiJquery(chai, chai.util, $);
+
+export { renderComponent, expect };
